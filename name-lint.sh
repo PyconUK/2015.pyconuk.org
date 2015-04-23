eee294dadb0e36ef9865f35f564a60b9ffc20f36
Sarah Mount <s.mount@wlv.ac.uk>
2015-04-23 17:34:52 +0100

    Tidy up: use arrays.

--- name-lint.sh
+++ name-lint.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-LINT0="$(egrep "Pycon UK" *.html *.rst -R)"
-LINT1="$(egrep "pycon UK" *.html *.rst -R)"
-LINT2="$(egrep "pyconUK" *.html *.rst -R)"
+LINT[0]="$(egrep -R "Pycon UK" *.html *.rst)"
+LINT[1]="$(egrep -R "pycon UK" *.html *.rst)"
+LINT[2]="$(egrep -R "pyconUK" *.html *.rst)"
 
 SUCCESS=0
 
-if [[ $LINT0 ]]; then
-    echo $LINT0
-    echo "Please spell the conference name PyCon UK."
-    SUCCESS=1
-fi
-if [[ $LINT1 ]]; then
-    echo $LINT1
-    echo "Please spell the conference name PyCon UK."
-    SUCCESS=1
-fi
-if [[ $LINT2 ]]; then
-    echo $LINT2
-    echo "Please spell the conference name PyCon UK."
-    SUCCESS=1
-fi
+for i in 0 1 2 ; do
+    if [[ ${LINT[$i]} ]]; then
+        echo "${LINT[$i]}"
+        echo "Please spell the conference name PyCon UK."
+        SUCCESS=1
+    fi
+done
 
 exit $SUCCESS
