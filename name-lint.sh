75a60662cbef295118b0f26e729a7f35958fd4bc
snim2 <mount.sarah@gmail.com>
2015-04-24 07:29:18 +0100

    Added PyConUK to list. Use recursive grep without dereference.

--- name-lint.sh
+++ name-lint.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-LINT[0]="$(egrep -R "Pycon UK" *.html *.rst)"
-LINT[1]="$(egrep -R "pycon UK" *.html *.rst)"
-LINT[2]="$(egrep -R "pyconUK" *.html *.rst)"
+LINT[0]="$(egrep "Pycon UK" -r *.html *.rst)"
+LINT[1]="$(egrep "pycon UK" -r *.html *.rst)"
+LINT[2]="$(egrep "pyconUK" -r *.html *.rst)"
+LINT[3]="$(egrep "PyConUK" -r *.html *.rst)"
 
 SUCCESS=0
 
-for i in 0 1 2 ; do
+for i in 0 1 2 3 ; do
     if [[ ${LINT[$i]} ]]; then
         echo "${LINT[$i]}"
         echo "Please spell the conference name PyCon UK."
