02b8fb2ae4ca6afafd65a62d26318e568e8153e8
snim2 <mount.sarah@gmail.com>
2015-04-24 19:40:37 +0100

    Simplifying. Switching to grep. Adding line numbers to output. Make shell agnostic.

--- name-lint.sh
+++ name-lint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-LINT[0]="$(egrep "Pycon UK" -r *.html *.rst)"
-LINT[1]="$(egrep "pycon UK" -r *.html *.rst)"
-LINT[2]="$(egrep "pyconUK" -r *.html *.rst)"
-LINT[3]="$(egrep "PyConUK" -r *.html *.rst)"
+LINT="$(grep -e "Pycon UK" -e "pycon UK" -e "pyconUK" -e "PyConUK" \
+        --line-number --recursive . \
+        --include "*.html" --include "*.rst")"
 
 SUCCESS=0
 
-for i in 0 1 2 3 ; do
-    if [[ ${LINT[$i]} ]]; then
-        echo "${LINT[$i]}"
-        echo "Please spell the conference name PyCon UK."
-        SUCCESS=1
-    fi
-done
+if ! -z $LINT  ;  then
+    echo "${LINT}"
+    echo "Please spell the conference name PyCon UK."
+    SUCCESS=1
+fi
 
 exit $SUCCESS
