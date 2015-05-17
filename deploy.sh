0cf750621d24d38684b1858b6c6064b8e2038d74
Peter Inglesby <peter.inglesby@gmail.com>
2015-05-17 22:34:45 +0100

    Configure credentials correctly

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,7 @@ if [[ $TRAVIS = "true" ]]; then
 	if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
 		echo "Deploying!"
 		git config credential.helper "store --file=.git/credentials"
-		echo "https://$GH_TOKEN:@github.com" > .git/credentials
-		echo "Credentials:"
-		cat .git/credentials
+		echo "https://PyConUK-user:$GH_TOKEN@github.com" > .git/credentials
 		git subtree push --prefix output https://PyConUK-user@github.com/PyconUK/pyconuk.org gh-pages
 		rm .git/credentials
 	else
