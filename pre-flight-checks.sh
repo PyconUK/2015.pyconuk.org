c4e544c4cd8ebd2be1d63418ccbeff4a029c5bb8
Peter Inglesby <peter.inglesby@gmail.com>
2015-05-17 22:48:32 +0100

    Add check that output directory has been checked in

--- pre-flight-checks.sh
+++ pre-flight-checks.sh
@@ -54,6 +54,19 @@ if [[ $? -eq 0 ]]; then
 	ERRORS+=("Conference name is not spelt correctly")
 fi
 
+###
+# Check that output directory is checked in.
+###
+
+if [[ $TRAVIS = "true" ]]; then
+	echo " *** Checking that output directory has been checked in."
+
+	git diff --quiet output
+	if [[ $? -ne 0 ]]; then
+		ERRORS+=("Uncommitted changes in output directory")
+	fi
+fi
+
 kill $WOK_PID
 
 if [[ ${#ERRORS[@]} -eq 0 ]]; then
