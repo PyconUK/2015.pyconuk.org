1ead5cd5befc740a903ebf50b99c264f6dab3dde
Peter Inglesby <peter.inglesby@gmail.com>
2015-05-17 22:08:24 +0100

    Don't leak $GH_TOKEN in travis logs

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,13 @@
 if [[ $TRAVIS = "true" ]]; then
 	if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
 		echo "Deploying!"
-		git subtree push --prefix output https://PyConUK-user:$GH_TOKEN@github.com/PyconUK/pyconuk.org gh-pages
+		# The output of this must be redirected to /dev/null to prevent leakage of $GH_TOKEN.
+		git subtree push --prefix output https://PyConUK-user:$GH_TOKEN@github.com/PyconUK/pyconuk.org gh-pages >/dev/null
+		if [[ $? -eq 0 ]]; then
+			echo "Deployed successfully!"
+		else
+			echo "Deployment failed!"
+		fi
 	else
 		echo "Not deploying!"
 	fi
