eb882e58b939396391c43af482ab6f4c577a025a
Peter Inglesby <peter.inglesby@gmail.com>
2015-05-17 22:22:42 +0100

    Try harder not to leak GitHub token

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,10 @@
 if [[ $TRAVIS = "true" ]]; then
 	if [[ $TRAVIS_BRANCH = "master" && $TRAVIS_PULL_REQUEST = "false" ]]; then
 		echo "Deploying!"
-		# The output of this must be redirected to /dev/null to prevent leakage of $GH_TOKEN.
-		git subtree push --prefix output https://PyConUK-user:$GH_TOKEN@github.com/PyconUK/pyconuk.org gh-pages >/dev/null
-		if [[ $? -eq 0 ]]; then
-			echo "Deployed successfully!"
-		else
-			echo "Deployment failed!"
-		fi
+		git config credential.helper "store --file=.git/credentials"
+		echo "https://$GH_TOKEN:@github.com" > .git/credentials
+		git subtree push --prefix output https://PyConUK-user@github.com/PyconUK/pyconuk.org gh-pages
+		rm .git/credentials
 	else
 		echo "Not deploying!"
 	fi
