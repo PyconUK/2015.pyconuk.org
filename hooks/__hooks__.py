6a92f15b671c80d7f6e70ec3e2093c8a92d3447f
snim2 <mount.sarah@gmail.com>
2015-09-09 18:12:31 +0100

    Remove the flat schedule. Needs to be put back when a bumpy schedule has been created.

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -4,15 +4,14 @@ Attach Python functions to wok hooks.
 '''
 
 import flat_schedule
-import guidebook
-import schedule_summary
 
 # The `hooks` dictionary that wok will import
 # See http://wok.mythmon.com/docs/hooks/
 hooks = {
-    'site.done': [
-            flat_schedule.create_flat_schedule,
-            guidebook.create_csv_schedule,
-            schedule_summary.create_summary_schedule,
-        ],
+# TODO: Put this back when the 2016 schedule has been created.
+#    'site.done': [
+#            flat_schedule.create_flat_schedule,
+#            guidebook.create_csv_schedule,
+#            schedule_summary.create_summary_schedule,
+#        ],
     }
