a5916b7bf7e2b4fd4e7b39fcefd748bfb1cb2662
Peter Inglesby <peter.inglesby@gmail.com>
2015-09-12 10:39:31 +0100

    Add hook to summarise the schedule
    This will make it easier to work out which talks have been rescheduled
    so that we can notify people when their talks have changed.

--- hooks/__hooks__.py
+++ hooks/__hooks__.py
@@ -5,10 +5,14 @@ Attach Python functions to wok hooks.
 
 import flat_schedule
 import guidebook
+import schedule_summary
 
 # The `hooks` dictionary that wok will import
 # See http://wok.mythmon.com/docs/hooks/
 hooks = {
-    'site.done': [flat_schedule.create_flat_schedule,
-        guidebook.create_csv_schedule],
+    'site.done': [
+            flat_schedule.create_flat_schedule,
+            guidebook.create_csv_schedule,
+            schedule_summary.create_summary_schedule,
+        ],
     }
